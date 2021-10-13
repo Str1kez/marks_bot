bd5a69e88858f9b4400431014b99577411f835f6
Str1kez <vazinov@yandex.ru>
2021-10-14 00:07:41 +0300

    fix utc param with parsing

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ if __name__ == '__main__':
     from aiogram import executor
     from loader import dp
 
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
